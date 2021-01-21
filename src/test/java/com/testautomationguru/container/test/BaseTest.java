5ac3f9fe013e761b490643c9723a7044fe1ef7de
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-01-21 17:33:08 +0530

    Update BaseTest.java

--- src/test/java/com/testautomationguru/container/test/BaseTest.java
+++ src/test/java/com/testautomationguru/container/test/BaseTest.java
@@ -37,12 +37,12 @@ public class BaseTest {
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
 ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
-        capabilities.setCapabilities(ChromeOptions.CAPABILITY,options);
+        capabilities.setCapability(ChromeOptions.CAPABILITY,options);
         
         
         driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
-        driver.manage().window.maximize();
-        google = new PageClass(driver);
+        driver.manage().window().maximize();
+      //  google = new PageClass(driver);
         
     }
 
