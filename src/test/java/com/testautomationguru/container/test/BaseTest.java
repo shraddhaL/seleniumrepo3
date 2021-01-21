e9ea50e4fccd80112c77ee590b1b952b8bc71ffa
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-01-21 17:22:29 +0530

    Update BaseTest.java

--- src/test/java/com/testautomationguru/container/test/BaseTest.java
+++ src/test/java/com/testautomationguru/container/test/BaseTest.java
@@ -18,7 +18,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-
+import org.openqa.selenium.remote.CapabilityType;
 public class BaseTest {
 
     protected WebDriver driver;
